0d65ad45bbb8ddb83ac07dfd8cc8437228db8d76
arzumify <jliwin98@danwin1210.de>
2025-01-08 14:18:25 +0000

    Added helper functions

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module git.ailur.dev/ailur/fg-nucleus-library
 
 go 1.23.3
 
-require github.com/google/uuid v1.6.0
+require (
+	git.ailur.dev/ailur/fg-library/v3 v3.5.0
+	github.com/google/uuid v1.6.0
+)
+
+require github.com/go-chi/chi/v5 v5.2.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package library
 
 import (
+	"crypto/ed25519"
+	library "git.ailur.dev/ailur/fg-library/v3"
 	"github.com/google/uuid"
+	"time"
 )
 
 type OAuthInformation struct {
@@ -26,4 +29,55 @@ type File struct {
 type Quota struct {
 	User  uuid.UUID `validate:"required"`
 	Bytes int64     `validate:"required"`
+}
+
+var (
+	StorageService = uuid.MustParse("00000000-0000-0000-0000-000000000003")
+	OAuthService   = uuid.MustParse("00000000-0000-0000-0000-000000000004")
+)
+
+func OAuthSignup(oauth OAuthInformation, information library.ServiceInitializationInformation) (OAuthResponse, error) {
+	message, err := information.SendAndAwaitISMessage(OAuthService, 1, oauth, time.Second*3)
+	if err != nil {
+		return OAuthResponse{}, err
+	}
+
+	return message.Message.(OAuthResponse), nil
+
+}
+
+func GetPublicKey(information library.ServiceInitializationInformation) (ed25519.PublicKey, error) {
+	message, err := information.SendAndAwaitISMessage(OAuthService, 2, nil, time.Second*3)
+	if err != nil {
+		return nil, err
+	}
+
+	return message.Message.(ed25519.PublicKey), nil
+}
+
+func GetOAuthHostname(information library.ServiceInitializationInformation) (string, error) {
+	message, err := information.SendAndAwaitISMessage(OAuthService, 0, nil, time.Second*3)
+	if err != nil {
+		return "", err
+	}
+
+	return message.Message.(string), nil
+}
+
+func InitializeOAuth(oauth OAuthInformation, information library.ServiceInitializationInformation) (oauthResponse OAuthResponse, pk ed25519.PublicKey, hostname string, err error) {
+	pk, err = GetPublicKey(information)
+	if err != nil {
+		return
+	}
+	hostname, err = GetOAuthHostname(information)
+	if err != nil {
+		return
+	}
+
+	oauthResponse, err = OAuthSignup(oauth, information)
+	if err != nil {
+		return
+	}
+
+	return
 }
